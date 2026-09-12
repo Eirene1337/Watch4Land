bf396c3a9867d01485c165167e7fe239d20aff1d
Eirene1337 <103354822+Eirene1337@users.noreply.github.com>
2026-09-12 20:34:31 +0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,12 +1,9 @@
 export const config = {
-  matcher: "/((?!maintenance.html|images|css|js|api).*)",
+  matcher: "/((?!images|css|js|api).*)",
 };
 
 export default function middleware(request) {
   if (process.env.MAINTENANCE_MODE === "true") {
-    return Response.redirect(
-      new URL("/maintenance.html", request.url),
-      307
-    );
+    return fetch(new URL("/maintenance.html", request.url));
   }
 }
